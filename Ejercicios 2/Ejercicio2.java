a5387f778da4e8475f51340b43e432e04ccb9710
Adrian Pelaz Medrano <71392163+AdrianPelaz@users.noreply.github.com>
2020-10-28 08:32:46 +0100

    Add files via upload

---
+++ Ejercicios 2/Ejercicio2.java
@@ -0,0 +1,13 @@
+/**
+ * Ejercicio 2
+ * 
+ * @author Adrian Pelaz Medrano
+ */
+ public class Ejercicio2 {
+   static class void main (String[] Args) {
+     
+     String miNombre = "Adrian Pelaz Medrano";
+     
+     System.out.println("Mi nombre es: " + miNombre);
+   }
+ }
---
+++ Ejercicios 2/Ejercicio3.java
@@ -0,0 +1,17 @@
+/**
+ * Ejercicio 3
+ * 
+ * @author Adrian Pelaz Medrano
+ */
+ public class Ejercicio3 {
+   static class void main (String[] Args) {
+     
+     String miNombre = "Adrian Pelaz Medrano";
+     String miDireccion = "Calle Luis De Rosales";
+     String miTelefono = "693004609";
+     
+     System.out.println("Mi nombre es: " + miNombre);
+     System.out.println("Mi direccion es: " + miDireccion);
+     System.out.println("Mi telefono es: " + miTelefono);
+   }
+ }
